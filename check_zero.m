0ea016ca5a10f89bcd0bdf3f324768dcec118217
unknown <jsjyspc@live.com>
2015-10-07 20:58:27 +0800

    single station

--- check_zero.m
+++ check_zero.m
@@ -1,14 +1,14 @@
-function [ flag ] = check_zero( npts, data )
+function [ flag ] = check_zero( S )
 %CHECK_ZERO Summary of this function goes here
 %   Detailed explanation goes here
 
 evl = 1e-9;
 eper = 0.15;
 
-rej_bool = bsxfun(@lt, abs(data), evl);
+rej_bool = bsxfun(@lt, abs(S.DATA1), evl);
 num_zero = sum(rej_bool);
 
-flag = (num_zero > (npts * eper));
+flag = (num_zero > (S.NPTS * eper));
 
 end
 
--- precondition.m
+++ precondition.m
@@ -4,11 +4,13 @@ distcomp.feature( 'LocalUseMpiexec', false );
 
 raw_dir = './seis_data/raw_data/';
 prestack_dir = './seis_data/pre_stack/';
+discard_dir = './seis_data/discard_data/';
 outpre = [prestack_dir, 'outpre.temp'];
 
 sta_file = dir([raw_dir, '*.SAC']);
 num_file = length(sta_file);
 nsta = num_file;
+segn = Inf;
 
 seg_seconds = 86400;
 freq_low = 0.01;
@@ -18,41 +20,53 @@ filter_order = 4;
 winlen = floor(0.5 / freq_low);
 halfwinlen = floor((winlen - 1) / 2);
 
-parfor ii = 1: num_file
-    S = readsac([raw_dir, sta_file(ii).name]);
+for ii = 1: num_file
+    dfname = [raw_dir, sta_file(ii).name];
+    S = readsac(dfname);
     [t, d] = getsacdata(S);
-    
-    if check_zero(S.NPTS, d) == 1
+    if check_zero(S)
         nsta = nsta - 1;
+        movefile(dfname, [discard_dir, S.FILENAME]);
         continue
-    end
+    end       
     
     [b, a] = butter(filter_order, [freq_low, freq_high] .* S.DELTA .* 2);
     d = filter(b, a, d);
     
-    d = absmean_norm(S.NPTS, d, halfwinlen);
-    
-    d = spectral_norm(d);
+%     d = absmean_norm(S.NPTS, d, halfwinlen);
+%     
+%     d = spectral_norm(d);
     
     seg = floor(S.NPTS * S.DELTA / seg_seconds);
-    S.NPTS = floor(seg_seconds / S.DELTA);
-    S.E = seg_seconds;
-    if ii == 1
-        outpreID = fopen(outpre, 'w');
-        fprintf(outpreID, '%d\n%d\n', nsta, seg);
-    else
-        outpreID = fopen(outpre, 'a');
-    end
+    segn = min(seg, segn);
     
-    for jj = 1: seg
-        S.DATA1 = d((jj - 1) * S.NPTS + 1: jj * S.NPTS);
+    S.FILENAME = dfname;
+    S.DATA1 = d;
+    writesac(S);
+end
+
+sta_file = dir([raw_dir, '*.SAC']);
+outpreID = fopen(outpre, 'w');
+fprintf(outpreID, '%d\n%d\n', nsta, segn);
+fclose(outpreID);
+
+for ii = 1: nsta
+    dfname = [raw_dir, sta_file(ii).name];
+    S = readsac(dfname);
+    Snew = S;
+    Snew.NPTS = floor(seg_seconds / S.DELTA);
+    Snew.E = seg_seconds;
+    
+    outpreID = fopen(outpre, 'a');    
+    for jj = 1: segn
+        Snew.DATA1 = S.DATA1((jj - 1) * Snew.NPTS + 1: jj * Snew.NPTS);
         name = ...
             [S.KNETWK, '_', S.KSTNM, '_', S.KCMPNM, '_', ...
             num2str(jj), '.SAC'];
-        S.FILENAME = ...
+        Snew.FILENAME = ...
             [prestack_dir, name];
         fprintf(outpreID, '%s\n', name);
-        status = writesac(S);
+        status = writesac(Snew);
     end
     fclose(outpreID);
 end
