9a2359b77a19e6a9f90b43c635dbf77adeae002e
unknown <jsjyspc@live.com>
2015-10-17 19:00:25 +0800

    find subfold in parenfold matching the regular expression

--- foldfind.m
+++ foldfind.m
@@ -1,8 +1,26 @@
 function [ subfold, substr ] = foldfind( parenfold, regexpr, opt )
 %FOLDFIND Summary of this function goes here
 %   Detailed explanation goes here
+if nargin < 2
+    ME = MException(...
+        'arguNum:LessThanRequired', ...
+        'Need at least 2 arguments, received %d!\n!', nargin);
+    throw(ME)
+end
+
+if ~exist(opt, 'var')
+    opt = 0;
+end
+
 opt = (strcmp(opt, 'match')) * 0 + (strcmp(opt, 'unmatch')) * 1;
-content = dir(parenfold);
+
+try
+    content = dir(parenfold);
+catch err
+    disp(err);
+    disp('Please convert cell to string array!')
+end
+
 foldname = {content([content.isdir] == 1).name};
 foldname = foldname(3: end);
 
