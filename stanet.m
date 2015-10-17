904cdc98adc44ff7bea077a554b5734dbdb96f7e
unknown <jsjyspc@live.com>
2015-10-17 18:51:23 +0800

    to be continued ...

--- stanet.m
+++ stanet.m
@@ -5,6 +5,7 @@ stanet= dir(net_dir);
 regexpr_sac = '\w*(SAC)\w*';
 regexpr_centre = '[\w-]*[0-9]+(m)';
 regexpr_dist = '[0-9]+m';
+regexpr_with_dash = '[\w-]*';
 precondtemp = 'precond.temp';
 
 for ii = 3: length(stanet)
@@ -16,61 +17,38 @@ for ii = 3: length(stanet)
     if exist(prestacknet_dir, 'dir') ~= 7
         mkdir(prestacknet_dir)
     end
-    precondtemp_name = strcat(prestacknet_dir, precondtemp);
-    
+        
     equip_dir = fullfile(net_dir, stanet(ii).name, filesep);
-    equip = dir(equip_dir);
-    equip_name = {equip([equip.isdir]==1).name};
-    equip_name = equip_name(3: end);
-    
-    comexpr_centre = [stanet(ii).name, regexpr_centre];
-    equip_prepheral = regexp(equip_name, comexpr_centre, 'match');
-    equip_prepheral = cell2mat ...
-        (cellfun(@isempty, equip_prepheral, 'UniformOutput', false));
-    equip_centre = equip_name(equip_prepheral == 1);
-    equip_prepheral = equip_name(equip_prepheral == 0);
-    
-    data_centre_dir = fullfile(equip_dir, char(equip_centre), filesep);
-    data_centre = dir(data_centre_dir);
-    data_centre_dir_next = ...
-        regexpi({data_centre(:).name}, regexpr_sac, 'match');
-    data_centre_dir_next = data_centre_dir_next(cell2mat(cellfun(...
-        @isempty, data_centre_dir_next, ...
-        'UniformOutput', false)) == 0);
-    data_centre_dir = fullfile( ...
-        data_centre_dir, char(data_centre_dir_next{:}), filesep);
+    comexpr_prepheral = [stanet(ii).name, regexpr_centre];    
+    [equip_centre_dir, ~] = ...
+        foldfind(equip_dir, comexpr_prepheral, 'unmatch');
+    [equip_prepheral_dir, equip_prepheral] = ...
+        foldfind(equip_dir, comexpr_prepheral, 'match');
     
     num_prepheral = numel(equip_prepheral);
     radius = zeros(num_prepheral, 1);
     
+    precondtemp_name = strcat(prestacknet_dir, precondtemp);
     tempID = fopen(precondtemp_name, 'w');
-    fprintf(tempID, '%s\n', data_centre_dir);
+    fprintf(tempID, '%d\n', num_prepheral + 1);
     
     for jj = 1: num_prepheral
         dist = regexp(equip_prepheral(jj), regexpr_dist, 'match');
         dist = char(dist{:});
         radius(jj) = str2double(dist(1: end - 1));
-        equip_rad_dir = ...
-            fullfile(equip_dir, char(equip_prepheral(jj)), filesep);
-        equip_seq = dir(equip_rad_dir);
-        equip_seq = {equip_seq([equip_seq.isdir] == 1).name};
-        equip_seq = equip_seq(3: end);
-        num_seq = numel(equip_seq);
+        
+        [equip_seq_dir, ~] = ... 
+            foldfind(equip_prepheral_dir{jj}, regexpr_with_dash, 'match');
+        
+        num_seq = numel(equip_seq_dir);
+        fprintf(tempID, '%d\n', num_seq);
         
         for kk = 1: num_seq
-            equip_seq_dir = ...
-                fullfile(equip_rad_dir, char(equip_seq(kk)), filesep);
-            data_centre = dir(equip_seq_dir);
-            equip_seq_dir_next = ...
-                regexpi({data_centre(:).name}, regexpr_sac, 'match');
-            equip_seq_dir_next = equip_seq_dir_next(cell2mat(cellfun(...
-                @isempty, equip_seq_dir_next, ...
-                'UniformOutput', false)) == 0);
-            equip_seq_dir = fullfile( ...
-                equip_seq_dir, char(equip_seq_dir_next{:}), filesep);
-            fprintf(tempID, '%s\n', equip_seq_dir);
+            [sac_fold, ~] = ... 
+                foldfind(equip_seq_dir{kk}, regexpr_sac, 'match');    
+            
         end
-    end       
+    end
     fclose(tempID);
 end
 
